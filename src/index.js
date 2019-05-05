991cdb9a8e2401b49c7e6946cd7948a495e8f04b
konstantin <ykos.web@gmail.com>
2019-05-05 22:54:43 +0300

    Basic components in App

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-console.log("hello world");
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const TodoList = () => {
+	return (
+		<ul>
+			<li>Learn React</li>
+			<li>Build Awesome App</li> 
+		</ul>
+	)
+}
+
+const el = (
+	<div>
+		<h1>My Todo List</h1>
+		<input placeholder="search"/>
+		<TodoList />
+	</div>
+);
+
+ReactDOM.render(el, document.getElementById('root'));
+
