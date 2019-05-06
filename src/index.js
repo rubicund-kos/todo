96762180164b26720afaef5a020cd04af3f7051a
konstantin <ykos.web@gmail.com>
2019-05-07 00:34:04 +0300

    Update index.js
    Add components

--- src/index.js
+++ src/index.js
@@ -2,21 +2,34 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const TodoList = () => {
+	const items = ['Drink tea', 'Learn React', 'Build awesome application']
 	return (
 		<ul>
-			<li>Learn React</li>
-			<li>Build Awesome App</li> 
+			<li>{items[0]}</li>
+			<li>{items[1]}</li> 
+			<li>{items[2]}</li> 
 		</ul>
-	)
+	);
+};
+
+const AppHeader = () => {
+	return <h1>My Todo List</h1>;
+};
+
+const SearchPanel = () => {
+	return <input placeholder="search"/>;
 }
 
-const el = (
-	<div>
-		<h1>My Todo List</h1>
-		<input placeholder="search"/>
-		<TodoList />
-	</div>
-);
+const App = () => {
+	return (
+		<div>
+			<AppHeader />
+			<SearchPanel />
+			<TodoList />
+		</div>
+	);
+};
 
-ReactDOM.render(el, document.getElementById('root'));
+
+ReactDOM.render(<App />, document.getElementById('root'));
 
